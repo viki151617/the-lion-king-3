ee86e02d994ec15439062bb6a2428c980719b22a
viki151617 <113185175+viki151617@users.noreply.github.com>
2022-12-18 21:30:56 +0200

    Update king.js

--- king.js
+++ king.js
@@ -1,11 +1,5 @@
-function zoom(event) {
-    let zoomer = event.currentTarget;
-
-    event.offsetX ? (offsetX = event.offsetX) : (offsetX = event.touches[0].pageX);
-    event.offsetY ? (offsetY = event.offsetY) : (offsetX = event.touches[0].pageX);
-
-    let x = offsetX / zoomer.offsetWidth  * 100;
-    let y = offsetY / zoomer.offsetHeight * 100;
-
-    zoomer.style.backgroundPosition = x + "% " + y +"%";
+function myFunction() {
+  var x = document.getElementById("myframe");
+  x.style.backgroundColor = "red";
 }
+
