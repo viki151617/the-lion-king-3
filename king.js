31f871e7920c8e2175dca7891417f6841f10f494
viki151617 <113185175+viki151617@users.noreply.github.com>
2022-12-20 19:11:43 +0200

    Update king.js

--- king.js
+++ king.js
@@ -1,6 +1,8 @@
 function myFunction() {
   var x = document.getElementById("myframe");
   x.style.backgroundColor = "red";
+}
+onclick="document.getElementById('demo').style.display='block'"
   
 // var c = document.getElementById("myframe1")//
  // var v = document.getElementById("myframe1")//
